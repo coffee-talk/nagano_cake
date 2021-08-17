e6a7d373a40b83961ed086a358fc3588982441a5
Kanayama-Risa <cosmos.24kix3@gmail.com>
2021-08-17 10:03:35 +0000

    [Update]デバイス作成完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :customers
+  root 'homes#top'
+  devise_for :admins, path: 'admins', controllers: {
+    sessions: "admins/sessions",
+    registrations: "admins/registrations"
+  }
+  devise_for :customers, path: 'customers', controllers: {
+    sessions: "public/sessions",
+    registrations: "public/registrations"
+  }
+  
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
