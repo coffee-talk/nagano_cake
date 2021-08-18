e5b80fdcd4f5825b6ab7d424927ec3df9e9a1712
Kanayama-Risa <cosmos.24kix3@gmail.com>
2021-08-18 06:49:51 +0000

    Revert "Merge pull request #12 from coffee-talk/risa"
    This reverts commit d00f9ae25df3821f9a75962b9394825a6f687ef2, reversing
    changes made to fcbb19f40fbce75fdf09871ae6ff681c3fd622bd.

--- config/routes.rb
+++ config/routes.rb
@@ -1,33 +1,3 @@
 Rails.application.routes.draw do
-  
-  devise_for :admins, path: 'admins', controllers: {
-    sessions: "admins/sessions",
-    registrations: "admins/registrations"
-  }
-  
-  devise_for :customers, path: 'customers', controllers: {
-    sessions: "public/sessions",
-    registrations: "public/registrations"
-  }
-  
-  namespace :admin do
-    get 'top' => 'homes#top'
-    resources :customers, only: [:index, :show, :edit, :update]
-    resources :products, only: [:index, :new, :create, :show, :edit, :update]
-    resources :genres, only: [:index, :create, :edit, :update]
-    resources :orders, only: [:show, :update]
-    resources :order_details, only: [:update]
-  end
-  
-  scope module: :customer do
-    root 'public/homes#top'
-    get 'about' => 'public/homes#about'
-    resources :customers, only: [:show, :edit, :update, :quit, :quit_update]
-    resources :products, only: [:index, :show]
-    resources :cart_items, only: [:index, :update, :destroy, :all_destroy, :create]
-    resources :orders, only: [:new, :confirm, :complete, :create, :index, :show]
-    resources :receivers, only: [:index, :edit, :create, :update, :destroy]
-  end
-  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
