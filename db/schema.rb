cdf5ab5478c240e92da4d42ae49e03e557c0fbfd
Kanayama-Risa <cosmos.24kix3@gmail.com>
2021-08-18 06:19:02 +0000

    routesの更新

--- db/schema.rb
+++ db/schema.rb
@@ -25,9 +25,9 @@ ActiveRecord::Schema.define(version: 2021_08_17_104020) do
   end
 
   create_table "cart_items", force: :cascade do |t|
-    t.integer "product_id", null: false
-    t.integer "customer_id", null: false
-    t.integer "amount", null: false
+    t.integer "product_id"
+    t.integer "customer_id"
+    t.integer "amount"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -38,14 +38,6 @@ ActiveRecord::Schema.define(version: 2021_08_17_104020) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "first_name", null: false
-    t.string "last_name", null: false
-    t.string "kana_first_name", null: false
-    t.string "kana_last_name", null: false
-    t.string "postal_code", null: false
-    t.string "address", null: false
-    t.string "phone_number", null: false
-    t.boolean "is_deleted", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_customers_on_email", unique: true
@@ -53,50 +45,50 @@ ActiveRecord::Schema.define(version: 2021_08_17_104020) do
   end
 
   create_table "genres", force: :cascade do |t|
-    t.string "name", null: false
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "ordered_products", force: :cascade do |t|
-    t.integer "order_id", null: false
-    t.integer "product_id", null: false
-    t.integer "amount", null: false
-    t.integer "making_status", default: 0, null: false
-    t.integer "price", null: false
+    t.integer "order_id"
+    t.integer "product_id"
+    t.integer "amount"
+    t.integer "making_status"
+    t.integer "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "orders", force: :cascade do |t|
-    t.integer "customer_id", null: false
-    t.integer "carriage", null: false
-    t.integer "payment_method", default: 0, null: false
-    t.string "postal_code", null: false
-    t.string "address", null: false
-    t.string "name", null: false
-    t.integer "status", null: false
-    t.integer "total_payment", null: false
+    t.integer "customer_id"
+    t.integer "carriage"
+    t.integer "payment_method"
+    t.string "postal_code"
+    t.string "address"
+    t.string "name"
+    t.integer "status"
+    t.integer "total_payment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "products", force: :cascade do |t|
-    t.string "name", null: false
-    t.text "introduction", null: false
-    t.string "image_id", null: false
-    t.integer "genre_id", null: false
-    t.integer "price", null: false
-    t.boolean "is_active", default: true, null: false
+    t.string "name"
+    t.text "introduction"
+    t.string "image_id"
+    t.integer "genre_id"
+    t.integer "price"
+    t.boolean "is_active"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "receivers", force: :cascade do |t|
-    t.integer "customer_id", null: false
-    t.string "postal_code", null: false
-    t.string "address", null: false
-    t.string "name", null: false
+    t.integer "customer_id"
+    t.string "postal_code"
+    t.string "address"
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
