995afca76a33e46e125d8c849829702867955153
yuki525 <entio777@gmail.com>
2021-08-19 08:01:20 +0000

    receiver

--- app/controllers/public/receivers_controller.rb
+++ app/controllers/public/receivers_controller.rb
@@ -1,23 +1,34 @@
 class Public::ReceiversController < ApplicationController
   
   def index
-    
+    @receiver = Receiver.new
+    @receivers = Receiver.all
   end 
   
   def create
-    
+    receiver = Receiver.new(receiver_params)
+    receiver.save
+    redirect_to  receivers_path
   end 
   
   def edit
+    @receiver = Receiver.find(params[:id])
     
   end 
   
   def update
-    
+    @receiver = Receiver.find(params[:id])
+    @receiver.update(receiver_params)
+    redirect_to receivers_path
   end 
   
   def destroy
     
+  end
+  
+  private
+  def receiver_params
+    params.require(:receiver).permit(:postal_code, :address, :name)
   end
   
 end
