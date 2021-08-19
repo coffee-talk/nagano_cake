19f2ae4bd132813e10836fc0198221e45a1bc5ea
Kanayama-Risa <cosmos.24kix3@gmail.com>
2021-08-19 03:40:59 +0000

    [update routes]

--- db/schema.rb
+++ db/schema.rb
@@ -38,14 +38,6 @@ ActiveRecord::Schema.define(version: 2021_08_17_104020) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "first_name", null: false
-    t.string "last_name", null: false
-    t.string "kana_first_name", null: false
-    t.string "kana_last_name", null: false
-    t.string "postal_code", null: false
-    t.string "address", null: false
-    t.string "phone_number", null: false
-    t.boolean "is_deleted", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_customers_on_email", unique: true
@@ -90,6 +82,18 @@ ActiveRecord::Schema.define(version: 2021_08_17_104020) do
     t.boolean "is_active", default: true, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "publics", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_publics_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_publics_on_reset_password_token", unique: true
   end
 
   create_table "receivers", force: :cascade do |t|
