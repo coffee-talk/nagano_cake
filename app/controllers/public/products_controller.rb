7732b0a0303bcff7c84ac59c802db8e420a849bc
Kanayama-Risa <cosmos.24kix3@gmail.com>
2021-08-20 02:51:30 +0000

    [update public products index]

--- app/controllers/public/products_controller.rb
+++ app/controllers/public/products_controller.rb
@@ -1,11 +1,16 @@
 class Public::ProductsController < ApplicationController
   
   def index
-    
+    @products = Product.where(is_sale: true).page(params[:page]).per(8)
   end 
   
   def show
-    
-  end 
+    @product = Product.find(params[:id])
+  end
+  
+  private
+  def product_params
+    params.require(:product).permit(:name, :price, :image)
+  end
   
 end
