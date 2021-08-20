c3e557fa0c9e91c062f5477118bd190ed9e57518
kyforprogram <kyforprogram@gmail.com>
2021-08-20 04:04:37 +0000

    add seeds.rb change customers_controller

--- app/controllers/admins/customers_controller.rb
+++ app/controllers/admins/customers_controller.rb
@@ -1,10 +1,27 @@
 class Admins::CustomersController < ApplicationController
+  before_action :authenticate_admin!
+  
   def index
+    @customers = Customer.all
   end
 
   def show
+    @customer = Customer.find(params[:id])
   end
 
   def edit
+    @customer = Customer.find(params[:id])
+  end
+  
+  def update
+    @customer = Customer.find(params[:id])
+    @customer.update(customer_params)
+    redirect_to @customer
+  end
+  
+  private
+  
+  def customer_params
+    params.require(:customer).permit(:first_name, :last_name, :kana_first_name, :kana_last_name, :postal_code, :address, :phone_number, :is_deleted)
   end
 end
