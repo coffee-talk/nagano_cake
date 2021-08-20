5297549640824a2f19a7c96a2314e31716b33516
EC2 Default User <ec2-user@ip-172-31-80-227.ec2.internal>
2021-08-20 05:28:10 +0000

    [update genres]

--- app/controllers/public/receivers_controller.rb
+++ app/controllers/public/receivers_controller.rb
@@ -1,36 +1,36 @@
 class Public::ReceiversController < ApplicationController
-
+  
   def index
     @receiver = Receiver.new
     @receivers = Receiver.all
-  end
-
+  end 
+  
   def create
-
+    
     receiver = Receiver.new(receiver_params)
     receiver.customer_id = current_customer.id
     receiver.save
     redirect_to  receivers_path
-  end
-
+  end 
+  
   def edit
     @receiver = Receiver.find(params[:id])
-
-  end
-
+    
+  end 
+  
   def update
     @receiver = Receiver.find(params[:id])
     @receiver.update(receiver_params)
     redirect_to receivers_path
-  end
-
+  end 
+  
   def destroy
-
+    
   end
-
+  
   private
   def receiver_params
     params.require(:receiver).permit(:postal_code, :address, :name)
   end
-
+  
 end
