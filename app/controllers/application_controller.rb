e7ad24da807b9ac36b684644ba5b1fe1dbf0cac1
kyforprogram <kyforprogram@gmail.com>
2021-08-23 10:32:10 +0000

    Update docs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  
+
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
@@ -8,12 +8,15 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :kana_first_name, :kana_last_name, :postal_code, :address, :phone_number])
   end
-  
+
   def after_inactive_sign_up_path_for(resource)
-    edit_customer_path(resource)          
-  end  
-  
+    edit_customer_path(resource)
+  end
+
   def after_sign_in_path_for(resource)
+    # admin
     root_path(resource)
   end
+
+
 end
