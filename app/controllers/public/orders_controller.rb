2c7394bdab90fba51f799284ff88dbb9c0e29dbe
Kanayama-Risa <cosmos.24kix3@gmail.com>
2021-08-24 07:40:47 +0000

    update

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,37 +1,28 @@
 class Public::OrdersController < ApplicationController
 
   def index
-
   end
 
   def show
-
   end
 
   def new
     @order = Order.new
     @address = current_customer
     @receiver = current_customer.receivers.all
-
-
   end
 
   def comfirm
-   @cart_items = CartItem.all
-   @payment_method = order_params[:payment_method]
-   
-   @address_option = order_params[:address_option]
+    @order = Order.new
+    @cart_items = current_customer.cart_items
+    @payment_method = order_params[:payment_method]
+    @address_option = order_params[:address_option]
    if @address_option == 0
-   @receiver = order_params[:payment_method]
-
-  end
-
-  def complete
-
+    @receiver = order_params[:payment_method]
+   end
   end
 
   def create
-
      @address_option = order_params[:address_option]
     if @address_option == 0
       @order = Order.new
@@ -40,13 +31,15 @@ class Public::OrdersController < ApplicationController
       @order.name = current_customer.name
     else
     end
-
+  end
+  
+  def complete
   end
 
   private
-  def order_params
-    params.require(:order).permit(:postal_code, :address, :name, :payment_method, :address_option, :order_address)
-  end
+    def order_params
+      params.require(:order).permit(:postal_code, :address, :name, :payment_method, :address_option, :order_address)
+    end
 
 
 end
