bd51e511db30584a69b4140a48aa409e7118b76f
Kanayama-Risa <cosmos.24kix3@gmail.com>
2021-08-26 08:07:43 +0000

    update public product show

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -2,10 +2,13 @@ class Public::OrdersController < ApplicationController
 
   def index
     @orders = Order.all
+    @product = Product.all
   end
 
   def show
     @order = Order.find(params[:id])
+    @ordered_products = @order.ordered_products
+    @order.carriage = 800
   end
 
   def new
@@ -56,13 +59,13 @@ class Public::OrdersController < ApplicationController
       @ordered_products.making_status = 0
       @ordered_products.price = cart_item.product.add_tax_price
       @ordered_products.save
-      current_customer.cart_items.destroy_all
     end
+      current_customer.cart_items.destroy_all
 
     if @order.save
-    redirect_to root_path
+      redirect_to orders_complete_path
     else
-    redirect_to cart_items_path
+      redirect_to orders_comfirm_path
     end
   end
 
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ApplicationRecord
   attachment :image
+  has_many :ordered_products, dependent: :destroy
   belongs_to :genre, foreign_key: 'genre_id'
 
   def add_tax_price
