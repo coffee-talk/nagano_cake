6e652b301c3d2d433b7ede5ed760cec4d06d2aac
kyforprogram <kyforprogram@gmail.com>
2021-08-28 03:36:20 +0000

    Update　fix

--- app/controllers/admins/order_details_controller.rb
+++ app/controllers/admins/order_details_controller.rb
@@ -12,9 +12,10 @@ class Admins::OrderDetailsController < ApplicationController
     if @ordered_product.making_status == 2 #binding.pryすると中身がintegerだった
       @ordered_product.order.update(status: '製作中')
       redirect_to admins_order_path(@ordered_product.order)
-    elsif @ordered_product.making_status == 3
+    elsif @ordered_product.making_status.count == 3 && @order_product.making_status.count
       #全部のmaking_statusが製作完了になれば配送準備中に切り替えるにはどうしたらいいか？？
-      @ordered_product.order.update(status: '発送準備中') 
+      # すべての注文されている製作ステータスが全部３なら、注文ステータスを変える
+      @ordered_product.order.update(status: '発送準備中')
       redirect_to admins_order_path(@ordered_product.order) and return # 戻り値を返すreturnメゾット
     else
       redirect_to admins_order_path(@ordered_product.order) and return
